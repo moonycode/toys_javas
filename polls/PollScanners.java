8e2c661e79e79bddf28408b9cb561768f01a363b
ppquokka <quokka2335@gmail.com>
2023-05-31 18:23:11 +0900

    KJW 답항 초안(집가서 할게요..)

--- polls/PollScanners.java
+++ polls/PollScanners.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;   // 담당: 답항 입력 실행
 
 public class PollScanners {
-    try {
-        
-    } catch (Exception e) {
-        // TODO: handle exception
-    } //return 0;
+    public static void main(String[] arge){
+        System.out.println("이름을 입력하세요");
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        } //return 0;
+    }
+    
 }
