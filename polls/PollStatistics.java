2344d12a73027915b734fe240c388e3a2d68bd9d
moonycode <chl93m@naver.com>
2023-05-31 18:22:05 +0900

    poll statistics

--- polls/PollStatistics.java
+++ polls/PollStatistics.java
@@ -1,3 +1,13 @@
 public class PollStatistics {
-  
-}
+  public int cnt = 0;
+
+  public String statistic (int Qnum){
+    for (this.cnt = 0; cnt < 5; cnt = cnt + 1) {
+      System.out.print((cnt + 1) + ".");
+      System.out.print("(" + totalAnswers[cnt] + ")");
+      if (Qnum != 5 - 1) {
+        System.out.print(",  ");
+      }
+
+  }
+  }
